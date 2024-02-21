1bb1597ece1763a6bc6683431153526ed0059f3b
Luis <luis.canelon.a@gmail.com>
2024-02-21 18:25:37 +0100

    recent changes

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,11 @@ module.exports = defineConfig({
     firefoxQty: "3",
     chromeQty: "2",
   },
-
+  video: true,
+  videoUploadOnPasses: false,
+  videoCompression: 32,
+  videoUploadOnFailures: true,
+  videosFolder: "cypress/videos",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
